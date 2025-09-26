cf6a6440c440d5667f8b9f4770836020d0784c56
Tripic12 <mishakazakov333@gmail.com>
2025-09-26 21:23:59 +0200

    Character move and rotation

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class CharacterControllerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rb;
+    [SerializeField]
+    private float _speed = 500f;
+    [SerializeField]
+    private float _rotationSpeed = 10f;
+    private Vector3 _move;
+    private Vector3 _mousePosition;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
+    void FixedUpdate()
+    {
+        _move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        rb.linearVelocity = _move * Time.deltaTime * _speed;
+    }
+
     void Update()
     {
-        
+
+        _mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 
+            Camera.main.transform.position.y - transform.position.y));
+
+        Vector3 lookDir = _mousePosition - transform.position;
+        lookDir.y = 0; // keep only horizontal rotation
+
+        Quaternion rot = Quaternion.LookRotation(lookDir);
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, rot, _rotationSpeed * Time.deltaTime));
     }
 }
