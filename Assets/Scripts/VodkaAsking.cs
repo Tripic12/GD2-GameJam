23519e7405ef18eb1997eea13d5e58a97706019d
Tripic12 <mishakazakov333@gmail.com>
2025-09-27 17:19:39 +0200

    VodkaAsking1

---
+++ Assets/Scripts/VodkaAsking.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class VodkaAsking : MonoBehaviour
+{
+    private List<GameObject> _drunkies = new List<GameObject>();
+    private int _assignTimer;
+    private bool _isAssigned;
+
+    private GameObject _obj;
+    private int _index;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _assignTimer++;
+
+        if (_assignTimer >= 5)
+        {
+            _drunkies[Random.Range(0, 4)] = ;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _index++;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            _obj = GameObject.Find($"VodkaImage{_index}");
+            _obj.SetActive(false);
+        }
+    }
+}
