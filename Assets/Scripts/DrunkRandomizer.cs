471b5d49d0f47042b7ef22e0e91ad1bb3d968edf
Tripic12 <mishakazakov333@gmail.com>
2025-09-27 18:43:30 +0200

    DrunkRandomizer in Beginning

---
+++ Assets/Scripts/DrunkRandomizer.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkRandomizer : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> _drunkies = new List<GameObject>();
+
+    void Start()
+    {
+        foreach (GameObject drunky in _drunkies)
+        {
+            Hitable hitable = drunky.GetComponent<Hitable>();
+            int randomValue = Random.Range(0, 14);
+            hitable.DrunknessAmount = randomValue;
+            hitable.DrunknessTargetValue = randomValue;
+        }
+    }
+}
