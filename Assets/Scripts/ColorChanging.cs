30c6b8eb395ed40ee9b63024c39d1f218a517a2a
LorimRolim <Tanghe.f.aaron@gmail.com>
2025-09-27 17:54:29 +0200

    smyh

--- Assets/Scripts/ColorChanging.cs
+++ Assets/Scripts/ColorChanging.cs
@@ -1,5 +1,3 @@
-using NUnit.Framework.Constraints;
-using TMPro;
 using UnityEngine;
 
 public class ColorChanging : MonoBehaviour
@@ -21,11 +19,11 @@ public class ColorChanging : MonoBehaviour
 
     //Status materials
     [SerializeField]
-    private Color _sober=Color.white;
+    private Color _sober = Color.white;
     [SerializeField]
-    private Color _drunk=Color.green;
+    private Color _drunk = Color.green;
     [SerializeField]
-    private Color _od=Color.red;
+    private Color _od = Color.red;
 
 
 
@@ -35,20 +33,24 @@ public class ColorChanging : MonoBehaviour
         _meshRenderer = GetComponent<MeshRenderer>();
         _hitable = GetComponent<Hitable>();
         _characterColor = _meshRenderer.material.color;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            _lerpTimer += Time.deltaTime / _duration;
-            _meshRenderer.material.color = Color.Lerp(_sober, _drunk, _lerpTimer);
-        }
 
-        if(_hitable.DrunknessAmount >= 20)
+        _lerpTimer = _hitable.DrunknessAmount / _hitable.MaxDrunkness;
+        _meshRenderer.material.color = Color.Lerp(_sober, _drunk, _lerpTimer);
+
+
+        if (_hitable.DrunknessAmount >= 20)
         {
             _isOD = true;
+        }
+        if (_isOD&&_hitable.DrunknessAmount<=20)
+        {
+            _isOD=false;
         }
         if (_isOD)
         {
@@ -63,7 +65,7 @@ public class ColorChanging : MonoBehaviour
                 _meshRenderer.material.color = _od;
             }
             //Game over if OD is too much
-            if(_timerOD >= 10 || _hitable.DrunknessAmount >= 25)
+            if (_timerOD >= 10 || _hitable.DrunknessAmount >= 25)
             {
                 Debug.Log("Game Over!");
             }
