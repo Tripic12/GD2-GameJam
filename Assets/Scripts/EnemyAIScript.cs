c61241f57fb01759d30566df938ec06e964ff0bf
Tripic12 <mishakazakov333@gmail.com>
2025-09-27 10:55:17 +0200

    smth

--- Assets/Scripts/EnemyAIScript.cs
+++ Assets/Scripts/EnemyAIScript.cs
@@ -9,25 +9,51 @@ public class EnemyAIScript : MonoBehaviour
     private NavMeshAgent _agent;
     [SerializeField]
     private GameObject _player;
+
+    private bool _isHit = false;
+    private float _lerpTime = 2f;
+    private float t;
+    private float f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Transform _playerTransform = _player.transform;
+        MeshRenderer _agentMesh = _agent.GetComponent<MeshRenderer>();
+
         if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out RaycastHit hitInfo))
             {
-                Vector3 pos = hitInfo.point;
-                pos = new Vector3(pos.x, 0, pos.z);
-                //_agent.SetDestination(pos);
-                _agent.SetDestination(_player.transform.position);
+
+                _agent.SetDestination(_playerTransform.position);
             }
+            if (_agent.transform.position == _playerTransform.position)
+            {
+                _agent.transform.position = _agent.transform.position;
+            }
+
+        }
+
+        if (_isHit)
+        {
+            t += Time.deltaTime / _lerpTime;
+            _agentMesh.material.color = Color.Lerp(Color.green, Color.red, t);
+        }     
+        if (!_isHit)
+        {
+            _agentMesh.material.color = Color.green;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            _isHit = true;
         }
     }
 }
